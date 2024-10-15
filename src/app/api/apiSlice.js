d125c45e877d472b6eab0e75f0cfe9ed26c2bc4f
dhia7 <dhianaija@gmail.com>
2024-10-15 14:49:24 +0100

    Ready to deploy

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { setCredentials } from '../../features/auth/authSlice'
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'https://technotes-kd2z.onrender.com',
+    baseUrl: 'https://technotes-backend-api-ptr1.onrender.com',
     credentials: 'include',
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token
